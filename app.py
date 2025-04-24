9342861990b2de750720bf6ed2ae5b62dcc5ae0c
Ana Robledo <166330650+Ansoroji@users.noreply.github.com>
2025-04-23 22:05:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,10 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.subheader("Aunque suena increíble de creer, el cuento Dinosaurio escrito por el guatemalteco Augusto Monterroso, es conocido como el cuento más corto que se ha escrito.")
+st.write('Cuando despertó'  
+         ' El dinosaurio todavia estaba allí' 
+         ' Augusto Monterroso'
         
         )
            
