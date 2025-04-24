64deebbac15d7b4b1811b61fab5c70731bd7cd40
Ana Robledo <166330650+Ansoroji@users.noreply.github.com>
2025-04-23 22:07:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,26 +46,12 @@ def text_to_speech(text, tld,lg):
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, text
 
-
-#display_output_text = st.checkbox("Verifica el texto")
-
 if st.button("convertir a Audio"):
      result, output_text = text_to_speech(text, 'com',lg)#'tld
      audio_file = open(f"temp/{result}.mp3", "rb")
      audio_bytes = audio_file.read()
      st.markdown(f"## Tú audio:")
      st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-     #if display_output_text:
-     
-     #st.write(f" {output_text}")
-    
-#if st.button("ElevenLAabs",key=2):
-#     from elevenlabs import play
-#     from elevenlabs.client import ElevenLabs
-#     client = ElevenLabs(api_key="a71bb432d643bbf80986c0cf0970d91a", # Defaults to ELEVEN_API_KEY)
-#     audio = client.generate(text=f" {output_text}",voice="Rachel",model="eleven_multilingual_v1")
-#     audio_file = open(f"temp/{audio}.mp3", "rb")
 
      with open(f"temp/{result}.mp3", "rb") as f:
          data = f.read()
